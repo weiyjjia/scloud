a02e132cd40a88f699db5450e51e915f2439c6f3
jiajwjw <jiajwjw@cn.ibm.com>
2016-08-25 10:31:58 +0800

    eandf

---
+++ e/src/main/java/c/d/e/App.java
@@ -0,0 +1,21 @@
+package c.d.e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by liaokailin on 16/4/28.
+ */
+@Configuration
+@EnableAutoConfiguration
+@RestController
+@EnableConfigServer
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ f/src/main/java/c/d/f/App.java
@@ -0,0 +1,26 @@
+package c.d.f;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by liaokailin on 16/4/28.
+ */
+@SpringBootApplication
+@RestController
+public class App {
+    @Value("${name:World!}")
+    String bar;
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello " + bar + "!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
